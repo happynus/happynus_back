f3fde576c18b4a5e3041ed7b357fed8ce22b675b
minj-L <wonder_minji@naver.com>
2022-04-10 01:02:14 +0900

    Fix : 듀티와 함께 근무자 정보도 알 수 있게 함

--- routes/shiftTable.js
+++ routes/shiftTable.js
@@ -15,6 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//전 사원 정보를 가져옴
+const getEmpData = async () => {
+  let empData = await axios.get(
+    "https://dutyapi-staging.azurewebsites.net/api/emp"
+  );
+  //console.log(shift.data);
+  return empData.data;
+};
+
 //사원들의 Night시프트와 OF시프트를 가져옴
 const getShift = async () => {
   let shift = await axios.get(
@@ -35,31 +44,35 @@ const getNDistint = async () => {
 };
 
 var empTotal = [];
-getNDistint().then((dist1) => {
-  dist1.map((dist2) => {
-    let keyName = dist2.empNo;
-    const date = [
+getEmpData().then((emp1) => {
+  emp1.map((emp2) => {
+    let teamNo = emp2.teamNo;
+    let position = emp2.position;
+    let empNo = emp2.empNo;
+    let empName = emp2.empName;
+    const duty = [
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0,
     ];
     getShift().then((shift1) => {
       shift1
-      .filter((shift2) => shift2.month === "05")
-      .map((shift2) => {
-        if (dist2.empNo == shift2.empNo) {
-          //console.log(Nshift.shiftCode);
-          //console.log(Nshift.date);
-          date.splice(parseInt(shift2.date) - 1, 1, shift2.shiftCode);
-          //console.log(date2);
-        }
-      });
-      let shiftN = { [keyName.toString()]: date };
-      empTotal.push(shiftN)
+        .filter((shift2) => shift2.month === "05")
+        .map((shift2) => {
+          if (emp2.empNo == shift2.empNo) {
+            //console.log(Nshift.shiftCode);
+            //console.log(Nshift.date);
+            duty.splice(parseInt(shift2.date) - 1, 1, shift2.shiftCode);
+            //console.log(date2);
+          }
+        });
+      //let shiftN = { [keyName.toString()]: date };
+      let shiftN = { teamNo : teamNo, position : position, empNo : empNo, empName : empName, duty : duty};
+      empTotal.push(shiftN);
     });
   });
-  app.get("/api/shiftCollo", (req, res) => {
-    //res.send(empTotal);
-    res.render("shiftTable", {emplist: empTotal});
+  app.get("/shiftTable", (req, res) => {
+    res.send(empTotal);
+    //res.render("shiftTable", { emplist: empTotal });
   });
 });
 
