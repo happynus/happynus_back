7938b3e798f3a708d7f4647e5b4410780c3e5d59
minj-L <wonder_minji@naver.com>
2022-04-10 14:01:28 +0900

    Refact : nightTurn app.get  수정중

--- routes/nightTurn.js
+++ routes/nightTurn.js
@@ -167,12 +167,9 @@ function calculEmpNum(res_, count) {
           i--;
         }
       }
-      //console.log(randomIndexArray)
 
       for (var randIndex of randomIndexArray) {
         var randValue = count[randIndex];
-        //console.log(randValue.empNo);
-
         var empInsert =
           'insert into currentduty (month, date, teamno, deptno, empno, shiftCode) values("' +
           nextMonth +
@@ -185,124 +182,307 @@ function calculEmpNum(res_, count) {
           '","' +
           randValue.empNo +
           "\",'N')";
-
-        //console.log(empInsert);
         connection.query(empInsert, function (err, result2) {
-          //console.log(result2)
-          console.log("데이터 insert");
+          console.log("일반근무자 배치완료");
         });
       }
     }
   }
 }
 
-app.get("/nightTurn", function (res, req) {
-  var mdCodeSelector =
-    "select empNo, teamNo, deptNo from emp where substring(statRule,3,1)=1";
-  var prgCodeSelector =
-    "select empNo, statRule from emp where substring(statRule, 1,1)=1";
-  var precepteeArrange =
-    "select empno, preceptor from emp where preceptor is not null";
+var mdCodeSelector =
+  "select empNo, teamNo, deptNo from emp where substring(statRule,3,1)=1";
+var prgCodeSelector =
+  "select empNo, statRule from emp where substring(statRule, 1,1)=1";
+var precepteeArrange =
+  "select empno, preceptor from emp where preceptor is not null";
 
-  //임산부 안내
-  connection.query(prgCodeSelector, function (err, result) {
-    for (var data of result) {
-      console.log("[임산부]", data.empNo);
-      console.log("**나이트 근무 배치 불가**");
+//임산부 안내
+connection.query(prgCodeSelector, function (err, result) {
+  for (var data of result) {
+    console.log("[임산부]", data.empNo);
+    console.log("**나이트 근무 배치 불가**");
+  }
+});
+
+//상근근무자 배치(공휴일 제외 주중근무)
+connection.query(mdCodeSelector, function (err, result) {
+  var workdays = [];
+  mdWorkingDays(year, nextMonth, function (wkndList) {
+    for (a = 0; a < wkndList.length; a++) {
+      workdays.push(wkndList[a]);
     }
-  });
-
-  //상근근무자 배치(공휴일 제외 주중근무)
-  connection.query(mdCodeSelector, function (err, result) {
-    var workdays = [];
-    mdWorkingDays(year, nextMonth, function (wkndList) {
-      for (a = 0; a < wkndList.length; a++) {
-        workdays.push(wkndList[a]);
-      }
-      for (var data of result) {
-        for (b = 0; b < workdays.length; b++) {
-          connection.query(
-            'insert into currentduty (month, date, teamNo, deptNo, empNo, shiftCode) values ("' +
-              nextMonth +
-              '","' +
-              ("00" + workdays[b].toString()).slice(-2) +
-              '",' +
-              data.teamNo +
-              "," +
-              data.deptNo +
-              "," +
-              "'" +
-              data.empNo +
-              "', 'MD')"
-          );
-        }
-      }
-      console.log("상근 근무자 배치완료");
-    });
-  });
-
-  //나이트킵, 일반근무자 프리셉터 코드를 가진 사람들을 레벨 코드에 따라 일정 비율로 나누어 N 배치한 코드
-  var teamNurse =
-    "SELECT deptNo, teamNo, empNo, levelCode, statRule FROM hospital.emp where statRule=00000 or statRule=00010 or statRule=01000;";
-
-  let count4 = new Array();
-  let count3 = new Array();
-  let count2 = new Array();
-  var count1 = new Array();
-
-  connection.query(teamNurse, function (err, result) {
-    //console.log(result);
     for (var data of result) {
-      if (data.levelCode == "1000") {
-        count4.push(data);
-      } else if (data.levelCode == "0001") {
-        count1.push(data);
-      } else if (data.levelCode == "0100") {
-        count3.push(data);
-      } else {
-        count2.push(data);
-      }
-    }
-
-    //console.log(typeof(count4.length));
-
-    var res_4 = empRateRound(count4, 0.2);
-    //console.log(typeof(res_4));
-    var res_3 = empRateRound(count3, 0.3);
-    //console.log(result3);
-    var res_2 = empRateRound(count2, 0.3);
-    //console.log(result2);
-    var res_1 = empRateRound(count1, 0.2);
-    //console.log(res_1);
-
-    calculEmpNum(res_4, count4);
-    calculEmpNum(res_3, count3);
-    calculEmpNum(res_2, count2);
-    calculEmpNum(res_1, count1);
-
-    //프리셉티 근무 배치
-    connection.query(precepteeArrange, function (err, result) {
-      for (var data of result) {
+      for (b = 0; b < workdays.length; b++) {
         connection.query(
-          "insert into currentduty (month,date,teamno,deptno,empno,shiftCode) select month, date, teamno, deptno," +
-            data.empno +
-            ", shiftCode from currentduty where empno=" +
-            data.preceptor
-        );
-        console.log(
-          "동일 듀티 배치를 확인하세요:",
-          data.preceptor,
-          "&",
-          data.empno
+          'insert into currentduty (month, date, teamNo, deptNo, empNo, shiftCode) values ("' +
+            nextMonth +
+            '","' +
+            ("00" + workdays[b].toString()).slice(-2) +
+            '",' +
+            data.teamNo +
+            "," +
+            data.deptNo +
+            "," +
+            "'" +
+            data.empNo +
+            "', 'MD')"
         );
       }
-      // 나이트 다음에 오프 배치하는 코드 전체 다 해당함 dao dbconn3
-      nightAfterOff();
+    }
+    console.log("상근 근무자 배치완료");
+  });
+});
+
+//나이트킵, 일반근무자 프리셉터 코드를 가진 사람들을 레벨 코드에 따라 일정 비율로 나누어 N 배치한 코드
+var teamNurse =
+  "SELECT deptNo, teamNo, empNo, levelCode, statRule FROM hospital.emp where statRule=00000 or statRule=00010 or statRule=01000;";
+
+let count4 = new Array();
+let count3 = new Array();
+let count2 = new Array();
+var count1 = new Array();
+
+connection.query(teamNurse, function (err, result) {
+  //console.log(result);
+  for (var data of result) {
+    if (data.levelCode == "1000") {
+      count4.push(data);
+    } else if (data.levelCode == "0001") {
+      count1.push(data);
+    } else if (data.levelCode == "0100") {
+      count3.push(data);
+    } else {
+      count2.push(data);
+    }
+  }
+
+  var res_4 = empRateRound(count4, 0.2);
+  //console.log(typeof(res_4));
+  var res_3 = empRateRound(count3, 0.3);
+  //console.log(result3);
+  var res_2 = empRateRound(count2, 0.3);
+  //console.log(result2);
+  var res_1 = empRateRound(count1, 0.2);
+  //console.log(res_1);
+
+  calculEmpNum(res_4, count4);
+  calculEmpNum(res_3, count3);
+  calculEmpNum(res_2, count2);
+  calculEmpNum(res_1, count1);
+
+  //프리셉티 근무 배치
+  connection.query(precepteeArrange, function (err, result) {
+    for (var data of result) {
+      connection.query(
+        "insert into currentduty (month,date,teamno,deptno,empno,shiftCode) select month, date, teamno, deptno," +
+          data.empno +
+          ", shiftCode from currentduty where empno=" +
+          data.preceptor
+      );
+      console.log(
+        "동일 듀티 배치를 확인하세요:",
+        data.preceptor,
+        "&",
+        data.empno
+      );
+    }
+    // 나이트 다음에 오프 배치하는 코드 전체 다 해당함 dao dbconn3
+    let today = new Date();
+
+    year = today.getFullYear(); //올해년도
+
+    var empNwork =
+      'select * from hospital.currentduty where empNo && shiftCode="N";';
+    connection.query(empNwork, function (err, result) {
+      var nShift = [];
+      for (var i = 0; i < result.length; i++) {
+        var nisHere =
+          "select exists (select shiftCode from hospital.currentduty where month=" +
+          result[i].month +
+          " && date=" +
+          String(parseInt(result[i].date) + 1).padStart(2, "0") +
+          " && empNo=" +
+          result[i].empNo +
+          ")" +
+          '"' +
+          result[i].month +
+          '"' +
+          '"' +
+          String(parseInt(result[i].date) + 1).padStart(2, "0") +
+          '"' +
+          '"' +
+          result[i].teamNo +
+          '"' +
+          '"' +
+          result[i].deptNo +
+          '"' +
+          '"' +
+          result[i].empNo +
+          '";';
+        //nShift.push(nisHere);
+        connection.query(nisHere, function (err, result2) {
+          if (Object.values(result2[0]) == 0) {
+            var str = Object.keys(result2[0]);
+            var month = str.toString().charAt(0) + str.toString().charAt(1);
+            var date = str.toString().charAt(3) + str.toString().charAt(4);
+            var teamNo =
+              str.toString().charAt(6) +
+              str.toString().charAt(7) +
+              str.toString().charAt(8) +
+              str.toString().charAt(9);
+            var deptNo =
+              str.toString().charAt(11) +
+              str.toString().charAt(12) +
+              str.toString().charAt(13);
+            var empNo =
+              str.toString().charAt(15) +
+              str.toString().charAt(16) +
+              str.toString().charAt(17) +
+              str.toString().charAt(18) +
+              str.toString().charAt(19) +
+              str.toString().charAt(20);
+
+            switch (month) {
+              case "01":
+              case "03":
+              case "05":
+              case "07":
+              case "08":
+              case "10":
+                if (date == "32") {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    String(parseInt(month) + 1).padStart(2, "0") +
+                    '", "01", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                } else {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    month +
+                    '", "' +
+                    date +
+                    '", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                }
+                break;
+              case "04":
+              case "06":
+              case "09":
+              case "11":
+                if (date == "31") {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    String(parseInt(month) + 1).padStart(2, "0") +
+                    '", "01", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                } else {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    month +
+                    '", "' +
+                    date +
+                    '", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                }
+                break;
+              case "02":
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                  if (date == "30") {
+                    var nightAndOff =
+                      'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                      String(parseInt(month) + 1).padStart(2, "0") +
+                      '", "01", "' +
+                      teamNo +
+                      '", "' +
+                      deptNo +
+                      '", "' +
+                      empNo +
+                      '", "/");';
+                  }
+                } else if (date == "29") {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    String(parseInt(month) + 1).padStart(2, "0") +
+                    '", "01", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                } else {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    month +
+                    '", "' +
+                    date +
+                    '", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                }
+                break;
+              case "12":
+                if (date == "32") {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("01", "01", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                } else {
+                  var nightAndOff =
+                    'INSERT INTO hospital.currentduty (month, date, teamNo, deptNo, empNo, shiftCode) VALUES ("' +
+                    month +
+                    '", "' +
+                    date +
+                    '", "' +
+                    teamNo +
+                    '", "' +
+                    deptNo +
+                    '", "' +
+                    empNo +
+                    '", "/");';
+                }
+                break;
+            }
+            connection.query(nightAndOff, function (err, result3) {
+              console.log("Off배치완료");
+              // app.get("/nightTurn", function (req, res) {
+              //   res.send("듀티 배치완료");
+              // });
+            });
+          }
+        });
+      }
     });
   });
-  //connection.connect(); //db접속
-  //nightAfterOff();
-  //connection.end();
 });
 
 module.exports = app;
